dfc420b5c3b5680faba3add2b1bce8a484b1a2ed
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-04-11 14:50:37 +0300

    add coverage command and make it a dep

--- budget-manager/tasks.py
+++ budget-manager/tasks.py
@@ -13,7 +13,13 @@ def test(ctx):
     ctx.run("pytest src", pty=True)
 
 
-# generoi testien haraumakattavuustulokset html-tiedostoon
+# luo testien kattavuustulokset
 @task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest", pty=True)
+
+
+# generoi testien haraumakattavuustulokset html-tiedostoon
+@task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
