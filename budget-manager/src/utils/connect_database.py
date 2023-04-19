fe19b66bbe0c487f1437a3479cc8efa9b5e76f25
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-04-19 13:35:59 +0300

    fix db path

--- budget-manager/src/utils/connect_database.py
+++ budget-manager/src/utils/connect_database.py
@@ -2,8 +2,9 @@ import os
 import sqlite3
 
 dirname = os.path.dirname(__file__)
+path = os.path.join(dirname, "..", "..", "data", "database.sqlite")
 
-connection = sqlite3.connect(os.path.join(dirname, "database.sqlite"))
+connection = sqlite3.connect(path)
 connection.row_factory = sqlite3.Row
 
 
