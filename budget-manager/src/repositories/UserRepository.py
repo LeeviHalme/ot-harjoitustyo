6978482c3d336e934059f35fad6fd7139a5786bd
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-05-01 21:15:38 +0300

    add more docstring

--- budget-manager/src/repositories/UserRepository.py
+++ budget-manager/src/repositories/UserRepository.py
@@ -2,11 +2,25 @@ from entities.User import User
 
 
 class UserRepository:
+    """Class used to query user information
+
+    Attributes:
+        _connection: Database connection
+    """
+
     def __init__(self, connection) -> None:
         self._connection = connection
 
     # get user by username
     def get_by_username(self, username: str):
+        """Get user object by their username
+
+        Args:
+            username (str): Input username
+
+        Returns:
+            User: Dictionary of a raw user object. (fixme: should probably return User)
+        """
         cursor = self._connection.cursor()
 
         cursor.execute(
@@ -23,6 +37,13 @@ class UserRepository:
 
     # create new user
     def create_new_user(self, name: str, username: str, password_hash: str) -> None:
+        """Creates a new user on the db
+
+        Args:
+            name (str): Input user's name
+            username (str): Input user's username
+            password_hash (str): Input user's hashed password
+        """
         # generate new UUID for user
         user_id = User.generate_id()
 
