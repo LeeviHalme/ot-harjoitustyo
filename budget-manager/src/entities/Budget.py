5fc7bde81f73044788d22d5732fbe9684e13e858
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-05-01 21:22:01 +0300

    add docstring to entities

--- budget-manager/src/entities/Budget.py
+++ budget-manager/src/entities/Budget.py
@@ -2,14 +2,30 @@ from uuid import uuid4
 
 
 class Budget:
-    def __init__(self, id: str, name: str, description: str, user_id: str) -> None:
-        self.id = id
+    """Describe the Budget object stored in the database
+
+    Attributes:
+        id: Budget's UUID
+        name: Budget's name
+        description: Budget's description
+        user_id: UUID of User who created this Budget
+    """
+
+    def __init__(
+        self, budget_id: str, name: str, description: str, user_id: str
+    ) -> None:
+        self.id = budget_id
         self.name = name
         self.description = description
         self.user_id = user_id
 
     @staticmethod
     def generate_id():
+        """Return a random UUID (version 4)
+
+        Returns:
+            str: Generated uuid
+        """
         return str(uuid4())
 
     def __str__(self) -> str:
--- budget-manager/src/entities/User.py
+++ budget-manager/src/entities/User.py
@@ -2,13 +2,26 @@ from uuid import uuid4
 
 
 class User:
-    def __init__(self, id: str, name: str, username: str) -> None:
-        self.id = id
+    """Describe the user object stored in the database without password
+
+    Attributes:
+        id: User's UUID
+        name: User's name
+        username: User's username
+    """
+
+    def __init__(self, user_id: str, name: str, username: str) -> None:
+        self.id = user_id
         self.name = name
         self.username = username
 
     @staticmethod
-    def generate_id():
+    def generate_id() -> str:
+        """Return a random UUID (version 4)
+
+        Returns:
+            str: Generated uuid
+        """
         return str(uuid4())
 
     def __str__(self) -> str:
