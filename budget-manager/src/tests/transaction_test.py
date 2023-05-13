491720e935696e63fbeb16c43ff67bba066e6981
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-05-13 15:56:52 +0300

    add missing tests for Transaction-entity

--- budget-manager/src/tests/transaction_test.py
+++ budget-manager/src/tests/transaction_test.py
@@ -9,6 +9,20 @@ class TestTransaction(unittest.TestCase):
 
         self.assertEqual(len(uid), 36)
 
+    # test getting date in correct format
+    def test_get_due_date(self):
+        trx = Transaction("test-id", "Test", 0, "2023-05-23 00:00:00")
+        date = trx.get_due_date()
+
+        self.assertEqual(date, "23.5.")
+
+    # test getting amount in correct format
+    def test_get_amount(self):
+        trx = Transaction("test-id", "Test", 1500, "2023-05-23 00:00:00")
+        amount = trx.get_amount()
+
+        self.assertEqual(amount, 15.0)
+
     # test str method
     def test_str(self):
         uid = Transaction.generate_id()
